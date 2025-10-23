c45980c731cec5aa7ffe67926892b582bc65533d
連亮禔 <lianliangzhi@lianxiangyideMacBook-Air-861.local>
2025-10-23 23:12:41 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 商品資料
+products = [
+    {
+        "name": "無線耳機",
+        "amazon_url": "https://www.amazon.com/dp/XXXXX?tag=demo-20",
+        "amazon_price": 59.99,
+        "local_price": 1790,
+        "image": "https://m.media-amazon.com/images/I/71p1kzZK%2BWL._AC_SL1500_.jpg"
+    },
+    {
+        "name": "藍牙音箱",
+        "amazon_url": "https://www.amazon.com/dp/YYYYY?tag=demo-20",
+        "amazon_price": 89.99,
+        "local_price": 2590,
+        "image": "https://m.media-amazon.com/images/I/81Xc7%2BDuDkL._AC_SL1500_.jpg"
+    }
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", products=products)
+
+if __name__ == "__main__":
+    app.run(debug=True)
