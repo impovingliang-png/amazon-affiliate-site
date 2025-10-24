b54b494c153e051858447216e7b9bd038b6951a5
連亮禔 <lianliangzhi@lianxiangyideMacBook-Air-861.local>
2025-10-24 09:07:17 +0800

    fix deployment settings for Render

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def index():
     return render_template("index.html", products=products)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)  # 改成 5001
+
